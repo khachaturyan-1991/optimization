7d1730860d51a14435cf82aa236592d610b9bf10
khachaturyan-1991 <dr.khachaturyan@gmail.com>
2026-02-19 19:39:46 +0100

    add CLI: train, benchmark, quantize

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
+import argparse
 import yaml
+
+from benchmark import Benchmark
+from quantize import Quantizer
 from train import Train
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--train", action="store_true", help="run training")
+    parser.add_argument("--benchmark", action="store_true", help="run benchmark")
+    parser.add_argument("--quantize", action="store_true", help="run quantization")
+    args = parser.parse_args()
 
     with open("config.yml", "r", encoding="utf-8") as f:
         config = yaml.safe_load(f)
 
-    trainer = Train(config)
-    trainer.run()
+    if args.train:
+        trainer = Train(config)
+        trainer.run()
 
+    if args.benchmark:
+        benchmarker = Benchmark(config)
+        benchmarker.run()
+
+    if args.quantize:
+        quantizer = Quantizer()
 
 if __name__ == "__main__":
     main()
